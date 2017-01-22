1ef2db306c5898fa5ee6b1d0ab53a6fa4fc2bdbc
leo412 <leelapherng@hotmail.com>
2017-01-22 23:09:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/smarttrafficapps/DBHandler.java
@@ -0,0 +1,17 @@
+package com.example.user.smarttrafficapps;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+/**
+ * Created by User on 1/12/2017.
+ */
+
+public class DBHandler {
+
+
+    
+
+
+
+
+}
---
+++ app/src/main/java/com/example/user/smarttrafficapps/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.user.smarttrafficapps;
+
+/**
+ * Created by User on 1/20/2017.
+ */
+
+public class MainActivity {
+}
